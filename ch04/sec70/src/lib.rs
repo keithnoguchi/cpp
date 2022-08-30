47470896e2cb59f291ac7cb42157812959396e3d
Keith Noguchi <keith@noguchi.us>
2022-08-30 09:10:04 -0700

    ch04/sec70: SpinLock::lock method
    With AtomicBool::compare_exchange_weak,
    Ordering and the spurious behavior.

--- ch04/sec70/src/lib.rs
+++ ch04/sec70/src/lib.rs
@@ -1,18 +1,39 @@
 //! SPDX-License-Identifier: GPL-2.0
 use std::cell::UnsafeCell;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 #[derive(Debug)]
 pub struct SpinLock<T> {
-    _lock: AtomicBool,
+    lock: AtomicBool,
     _data: UnsafeCell<T>,
+}
+
+pub struct SpinLockGuard<'a, T> {
+    spin_lock: &'a SpinLock<T>,
 }
 
 impl<T> SpinLock<T> {
     pub fn new(v: T) -> Self {
         Self {
-            _lock: AtomicBool::new(false),
+            lock: AtomicBool::new(false),
             _data: UnsafeCell::new(v),
         }
+    }
+
+    pub fn lock(&self) -> SpinLockGuard<T> {
+        loop {
+            // spinning for the lock.
+            while self.lock.load(Ordering::Relaxed) {}
+
+            if let Ok(_) = self.lock.compare_exchange_weak(
+                false,
+                true,
+                Ordering::Acquire, // ordering for success
+                Ordering::Relaxed, // ordering for failure
+            ) {
+                break;
+            }
+        }
+        SpinLockGuard { spin_lock: self }
     }
 }

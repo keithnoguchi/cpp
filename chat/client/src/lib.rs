25ab4f34a3b7e6ba1e4d132ccba295644b81ba4b
Keith Noguchi <hack@noguchi.us>
2022-10-13 17:46:29 -0700

    chat/client: Parse the command line to the Request

--- chat/client/src/lib.rs
+++ chat/client/src/lib.rs
@@ -2,17 +2,30 @@
 use async_std::io::prelude::BufReadExt;
 use async_std::io::{self, BufReader};
 use async_std::stream::StreamExt;
+use protocol::Request;
 use std::error::Error;
 use std::result;
 
 pub type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
 
 pub async fn sender(_addr: &str) -> Result<()> {
+    println!(
+        "join <GROUP>\n\
+         post <GROUP> MSG...\n\
+         type Control-D to close the connection.",
+    );
     let mut reader = BufReader::new(io::stdin()).lines();
 
     while let Some(line) = reader.next().await {
         let line = line?;
-        println!("{line}");
+        let req = match Request::try_from(&line[..]) {
+            Err(e) => {
+                eprintln!("{e}");
+                continue;
+            },
+            Ok(req) => req,
+        };
+        println!("{req:?}");
     }
     Ok(())
 }
--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -3,12 +3,10 @@ use async_std::task;
 use client::sender;
 use client::Result;
 
+const ADDR: &str = "::1:8080";
+
 fn main() -> Result<()> {
-    let mut args = std::env::args();
-    let progname = args.next().unwrap();
-    let addr = args
-        .next()
-        .unwrap_or_else(|| panic!("Usage: {progname} ADDR"));
+    let addr = std::env::args().nth(1).unwrap_or_else(|| ADDR.to_string());
 
     task::block_on(sender(&addr))
 }

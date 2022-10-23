80aac2fb2d40c90edbd7fbea67022fdcca66c61b
Keith Noguchi <hack@noguchi.us>
2022-10-23 07:39:37 -0700

    chat/group: join and post function
    This will complete the Group type.

--- chat/group/src/lib.rs
+++ chat/group/src/lib.rs
@@ -1,8 +1,51 @@
 //! Chat group crate
+use async_std::task::spawn;
+use connection::Outbound;
+use protocol::Response;
 use std::sync::Arc;
-use tokio::sync::broadcast::Sender;
+use tokio::sync::broadcast::error::{RecvError, SendError};
+use tokio::sync::broadcast::{channel, Receiver, Sender};
+
+const NR_CHANNEL_BOUNDED_SIZE: usize = 1_000;
+
+type Message = Arc<String>;
 
 pub struct Group {
     name: Arc<String>,
-    sender: Sender<Arc<String>>,
+    tx: Sender<Message>,
+}
+
+impl Group {
+    pub fn new(name: Arc<String>) -> Self {
+        let (tx, _rx) = channel(NR_CHANNEL_BOUNDED_SIZE);
+        Self { name, tx }
+    }
+
+    pub fn join(&self, tx: Outbound) {
+        spawn(receiver(self.name.clone(), tx, self.tx.subscribe()));
+    }
+
+    pub fn post(&self, msg: Message) {
+        match self.tx.send(msg) {
+            Err(SendError(_)) => eprintln!("{}: no active friends", self.name),
+            Ok(n) => println!("{}: sent to {n} friends", self.name),
+        }
+    }
+}
+
+async fn receiver(group_name: Arc<String>, tx: Outbound, mut rx: Receiver<Message>) {
+    loop {
+        let msg = match rx.recv().await {
+            Err(RecvError::Closed) => break,
+            Err(RecvError::Lagged(n)) => panic!("{group_name}: lagged {n}"),
+            Ok(msg) => Response::Message {
+                group_name: group_name.clone(),
+                message: msg,
+            },
+        };
+        if let Err(e) = tx.send(msg).await {
+            // let's just panic for now.
+            panic!("{group_name}: {e}");
+        }
+    }
 }

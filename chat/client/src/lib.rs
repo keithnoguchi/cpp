afb753ad7a93620a7a2c189e14e935b1e41eb358
Keith Noguchi <hack@noguchi.us>
2022-10-26 21:45:55 -0700

    chat/client: TX/RX mux

--- chat/client/src/lib.rs
+++ chat/client/src/lib.rs
@@ -1,26 +1,40 @@
 //! Asynchronous Chat Client
 use async_std::io::prelude::BufReadExt;
-use async_std::io::{self, BufReader};
+use async_std::io::{stdin, BufReader};
 use async_std::net::TcpStream;
 use async_std::stream::StreamExt;
+use async_std::task::sleep;
+use futures_lite::FutureExt;
 use protocol::Request;
 use std::error::Error;
 use std::result;
+use std::time::Duration;
 
 pub type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
 
-pub async fn sender(addr: &str) -> Result<()> {
+pub async fn run(addr: &str) -> Result<()> {
+    let s = TcpStream::connect(addr).await?;
+
+    sender(s.clone()).race(receiver(s)).await
+}
+
+async fn receiver(_s: TcpStream) -> Result<()> {
+    loop {
+        sleep(Duration::from_millis(100)).await;
+    }
+}
+
+async fn sender(s: TcpStream) -> Result<()> {
     println!(
         "join <GROUP>\n\
          post <GROUP> MSG...\n\
          type Control-D to close the connection.",
     );
-    let s = TcpStream::connect(addr).await?;
-    let mut reader = BufReader::new(io::stdin()).lines();
 
-    while let Some(line) = reader.next().await {
+    let mut rx = BufReader::new(stdin()).lines();
+    while let Some(line) = rx.next().await {
         let req = Request::try_from(line?)?;
-        packet::send(s.clone(), req).await?;
+        packet::send(&s, req).await?;
     }
     Ok(())
 }
--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -1,12 +1,15 @@
 //! Asynchronous Chat Client
 use async_std::task;
-use client::sender;
-use client::Result;
+use std::path::PathBuf;
 
 const ADDR: &str = "localhost:8080";
 
-fn main() -> Result<()> {
-    let addr = std::env::args().nth(1).unwrap_or_else(|| ADDR.to_string());
+fn main() -> client::Result<()> {
+    let mut args = std::env::args();
+    let progname = args.next().map(PathBuf::from).unwrap();
+    let addr = args.next().unwrap_or_else(|| ADDR.to_string());
 
-    task::block_on(sender(&addr))
+    println!("{:?}: connect to {addr}", progname.file_name().unwrap());
+
+    task::block_on(client::run(&addr))
 }

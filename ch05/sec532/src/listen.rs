800dd2bb73f6bf580930420507b320071e409572
Keith Noguchi <hack@noguchi.us>
2022-11-26 09:01:29 -0800

    5.3.2 I/O Selector
    Error handling in the Future implementation
    of Acceptor.
    
    The next PR should wrap the entire section
    with the actual echo server up and running.

--- ch05/sec532/src/listen.rs
+++ ch05/sec532/src/listen.rs
@@ -1,11 +1,17 @@
 //! 5.3.2 I/O Selector with epoll(7)
 use crate::{Result, Selector};
+use nix::sys::epoll::EpollFlags;
+use std::future::Future;
+use std::io::ErrorKind::WouldBlock;
 use std::net::{TcpListener, ToSocketAddrs};
+use std::os::unix::io::AsRawFd;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 pub struct Listener {
-    _internal: TcpListener,
-    _selector: Arc<Selector>,
+    internal: TcpListener,
+    selector: Arc<Selector>,
 }
 
 impl Listener {
@@ -15,17 +21,35 @@ impl Listener {
     {
         let internal = TcpListener::bind(addrs)?;
         internal.set_nonblocking(true)?;
-        Ok(Self {
-            _internal: internal,
-            _selector: selector,
-        })
+        Ok(Self { internal, selector })
     }
 
     pub fn accept(&self) -> Acceptor {
-        Acceptor { _listener: self }
+        Acceptor { listener: self }
     }
 }
 
 pub struct Acceptor<'a> {
-    _listener: &'a Listener,
+    listener: &'a Listener,
+}
+
+impl<'a> Future for Acceptor<'a> {
+    type Output = Result<String>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.listener.internal.accept() {
+            Ok(_) => todo!(),
+            Err(e) if e.kind() == WouldBlock => {
+                match self.listener.selector.register(
+                    EpollFlags::EPOLLIN,
+                    self.listener.internal.as_raw_fd(),
+                    cx.waker().clone(),
+                ) {
+                    Err(e) => Poll::Ready(Err(e)),
+                    Ok(_) => Poll::Pending,
+                }
+            }
+            Err(e) => Poll::Ready(Err(e)?),
+        }
+    }
 }

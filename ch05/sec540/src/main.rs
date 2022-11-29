96456968ce490a0388087de2f2f31dea610b0f81
Keith Noguchi <hack@noguchi.us>
2022-11-29 06:18:59 -0800

    5.4.0 Echo Server with tokio
    Using the tracing crate instead of panics.

--- ch05/sec540/src/main.rs
+++ ch05/sec540/src/main.rs
@@ -6,6 +6,7 @@ use std::str::FromStr;
 use std::sync::{atomic::AtomicBool, Arc};
 use std::thread::spawn;
 use std::time::Duration;
+use tracing::{error, info};
 
 const NR_TIMEOUT: Duration = Duration::from_secs(10);
 const NR_LISTEN_ADDR: &str = "127.0.0.1";
@@ -14,6 +15,7 @@ const NR_LISTENERS: usize = 10;
 const NR_SPAWNERS: usize = 3;
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let mut args = std::env::args();
     let progname = args.next().map(PathBuf::from).unwrap();
     let nr_timeout = args
@@ -43,7 +45,7 @@ fn main() {
         .and_then(|v| usize::from_str(v).ok())
         .unwrap_or(NR_SPAWNERS);
 
-    println!(
+    info!(
         "{:?}: listen on {}:{}..{} with {:?} timeout",
         progname.file_name().unwrap(),
         nr_addr,
@@ -65,13 +67,16 @@ fn main() {
                 for port in ports {
                     let addr = SocketAddr::new(nr_addr, port as u16);
                     let server = match Server::bind(addr) {
-                        Err(e) => panic!("{e}"),
+                        Err(e) => {
+                            error!(error = %e, "server creation");
+                            return;
+                        }
                         Ok(server) => server,
                     };
                     let done = done1.clone();
                     tokio::spawn(async move {
                         if let Err(e) = server.run(done).await {
-                            panic!("{e}");
+                            error!(error = %e, "server execution");
                         }
                     });
                 }
@@ -80,7 +85,7 @@ fn main() {
 
     for spawner in spawners.drain(..) {
         if let Err(e) = spawner.join() {
-            eprintln!("{e:?}");
+            error!(error = ?e, "spawner crash");
         }
     }
 }

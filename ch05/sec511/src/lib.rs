ca44cfed22e22f9c4430a8ba93144f68817690cb
Keith Noguchi <hack@noguchi.us>
2022-12-31 10:40:16 -0800

    sec511: s/tasks/workers/g

--- ch05/sec511/src/lib.rs
+++ ch05/sec511/src/lib.rs
@@ -14,9 +14,9 @@ use tracing::instrument;
 pub async fn serve(addr: impl ToSocketAddrs + Debug) -> io::Result<()> {
     let listener = TcpListener::bind(addr).await?;
 
-    let mut tasks = vec![];
+    let mut workers = vec![];
     while let Some(result) = listener.incoming().next().await {
-        tasks.push(smol::spawn(async move {
+        workers.push(smol::spawn(async move {
             let stream = result?;
             let mut reader = BufReader::new(stream.clone()).lines();
             let mut writer = BufWriter::new(stream);
@@ -32,8 +32,8 @@ pub async fn serve(addr: impl ToSocketAddrs + Debug) -> io::Result<()> {
     }
     // this won't happen unless the tcp connection is closed somehow
     // above.
-    for task in tasks {
-        task.await?;
+    for worker in workers {
+        worker.await?;
     }
     Ok(())
 }
